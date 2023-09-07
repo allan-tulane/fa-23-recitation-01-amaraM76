0bd20810abba9afc4e5f0196b1d550f3cf7af3d9
amaraM76 <136095008+amaraM76@users.noreply.github.com>
2023-09-06 22:29:57 -0500

    AM added in functions

--- test_main.py
+++ test_main.py
@@ -11,8 +11,11 @@ def test_linear_search():
 
 def test_binary_search():
 	assert binary_search([1,2,3,4,5], 5) == 4
+	assert binary_search([1,2,3,4,5], 4) == 3
+	assert binary_search([1,2,3,4,5], 3) == 2
 	assert binary_search([1,2,3,4,5], 1) == 0
 	assert binary_search([1,2,3,4,5], 6) == -1
+	
 	### TODO: add two more tests here.
 
 	###
@@ -25,3 +28,5 @@ def test_compare_search():
 	assert res[1][0] == 100
 	assert res[0][1] < 1
 	assert res[1][1] < 1
+
+
